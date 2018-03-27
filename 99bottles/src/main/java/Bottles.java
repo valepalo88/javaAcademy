62cdb393ab99e84b9d7b9cf7a7e70a40ade56e3b
nolanof <francesco.nolano@yoox.com>
2018-03-27 18:26:14 +0200

    Calculate remaining drinks

--- 99bottles/src/main/java/Bottles.java
+++ 99bottles/src/main/java/Bottles.java
@@ -3,44 +3,36 @@ import java.io.FileNotFoundException;
 import java.io.PrintStream;
 
 class Bottles {
+
+    static final int maxNumber = 99;
     public static void main(String args[]) throws FileNotFoundException {
-        generateOutput(new PS(null));
-    }
-
-    private static class PS extends PrintStream {
-        String totale = "";
-
-        public PS(File file) throws FileNotFoundException {
-            super(file);
-        }
-
-        @Override
-        public void print(String s) {
-            totale = totale + s;
-        }
-
-        @Override
-        public void println(String s) {
-            totale = totale + s;
-        }
+        generateOutput(System.out);
     }
 
     public static void generateOutput(PrintStream out) {
-        final int maxNumber = 99;
+
         for (int number = maxNumber; number > -1; number--) {
 
             if (number == 0) {
                 out.print(number + getContainer(number) + " of " + getDrink() + " on the wall,");
                 out.println(number + getContainer(number) + " of " + getDrink() + ",");
                 out.print("Go to the store, buy some more,");
-                out.println(maxNumber + getContainer(maxNumber) + " of " + getDrink() + " on the wall.\r\n");
+                out.println(remainingDrinks(number) + getContainer(remainingDrinks(number)) + " of " + getDrink() + " on the wall.\r\n");
             } else {
                 out.print(number + getContainer(number) + " of " + getDrink() + " on the wall,");
                 out.println(number + getContainer(number) + " of " + getDrink() + ",");
                 out.print("Take one down, pass it around,");
-                out.println(number - 1 + getContainer(number - 1) + " of " + getDrink() + " on the wall.\r\n");
+                out.println(remainingDrinks(number) + getContainer(remainingDrinks(number)) + " of " + getDrink() + " on the wall.\r\n");
             }
         }
+    }
+
+    private static int remainingDrinks(int n)
+    {
+        if (n == 0){
+            return maxNumber;
+        }
+        return n-1;
     }
 
     private static String getDrink() {
