b1af98cfc7858e861add11799c4d81539072bd81
Giulio Caccin <giulio.caccin@ynap.com>
2018-03-27 11:41:57 +0200

    add expected from resources

--- 99bottles/src/test/java/test.java
+++ 99bottles/src/test/java/test.java
@@ -2,17 +2,22 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class test {
     @Test
-    public void test(){
+    public void test() throws IOException {
         ByteArrayOutputStream stream = new ByteArrayOutputStream();
         PrintStream printStream = new PrintStream(stream);
         new Bottles().generateOutput(printStream);
-        String output = new String(stream.toByteArray());
+        String actual = new String(stream.toByteArray());
 
-        Assert.assertNotNull(output);
-        Assert.assertTrue(output.length() > 0);
+        String expected = new String(Files.readAllBytes(Paths.get("src/main/resources/bottles.txt")));
+
+        Assert.assertEquals(actual, expected);
+
     }
 }
