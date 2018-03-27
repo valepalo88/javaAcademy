9bf31c148fbd979ee616b070a2ff0ac49148795a
nolanof <francesco.nolano@yoox.com>
2018-03-27 18:28:19 +0200

    Introduce action

--- 99bottles/src/main/java/Bottles.java
+++ 99bottles/src/main/java/Bottles.java
@@ -5,6 +5,7 @@ import java.io.PrintStream;
 class Bottles {
 
     static final int maxNumber = 99;
+
     public static void main(String args[]) throws FileNotFoundException {
         generateOutput(System.out);
     }
@@ -16,23 +17,28 @@ class Bottles {
             if (number == 0) {
                 out.print(number + getContainer(number) + " of " + getDrink() + " on the wall,");
                 out.println(number + getContainer(number) + " of " + getDrink() + ",");
-                out.print("Go to the store, buy some more,");
+                out.print(action(number));
                 out.println(remainingDrinks(number) + getContainer(remainingDrinks(number)) + " of " + getDrink() + " on the wall.\r\n");
             } else {
                 out.print(number + getContainer(number) + " of " + getDrink() + " on the wall,");
                 out.println(number + getContainer(number) + " of " + getDrink() + ",");
-                out.print("Take one down, pass it around,");
+                out.print(action(number));
                 out.println(remainingDrinks(number) + getContainer(remainingDrinks(number)) + " of " + getDrink() + " on the wall.\r\n");
             }
         }
     }
 
-    private static int remainingDrinks(int n)
-    {
-        if (n == 0){
+    private static String action(int n) {
+        if (n == 0)
+            return "Go to the store, buy some more,";
+        return "Take one down, pass it around,";
+    }
+
+    private static int remainingDrinks(int n) {
+        if (n == 0) {
             return maxNumber;
         }
-        return n-1;
+        return n - 1;
     }
 
     private static String getDrink() {
