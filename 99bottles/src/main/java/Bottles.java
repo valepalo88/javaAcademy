9a5b92d25dceb083055e96ebe4e9fbb826286454
nolanof <francesco.nolano@yoox.com>
2018-03-27 17:08:42 +0200

    Convert ternary operator to if then else

--- 99bottles/src/main/java/Bottles.java
+++ 99bottles/src/main/java/Bottles.java
@@ -14,12 +14,17 @@ class Bottles {
                 out.println(beers + " bottle" + s + " of beer,");
                 out.print("Go to the store, buy some more,");
                 out.println("99 bottles of beer on the wall.\r\n");
-                
+
             } else {
                 out.print(beers + " bottle" + s + " of beer on the wall,");
                 out.println(beers + " bottle" + s + " of beer,");
                 out.print("Take one down, pass it around,");
-                s = (beers - 1 == 1) ? "" : "s";
+                if (beers - 1 == 1) {
+                    s = "";
+                }
+                else {
+                    s = "s";
+                }
                 out.println(beers - 1 + " bottle" + s + " of beer on the wall.\r\n");
             }
         }
