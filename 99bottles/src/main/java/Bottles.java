95ed6c046bf27ff459299dd54b7441cc9b93f440
nolanof <francesco.nolano@yoox.com>
2018-03-27 15:57:49 +0200

    Duplicate code

--- 99bottles/src/main/java/Bottles.java
+++ 99bottles/src/main/java/Bottles.java
@@ -8,14 +8,18 @@ class Bottles {
     public static void generateOutput(PrintStream out) {
         String s = "s";
         for (int beers = 99; beers > -1; ) {
-            out.print(beers + " bottle" + s + " of beer on the wall,");
-            out.println(beers + " bottle" + s + " of beer,");
+
             if (beers == 0) {
+                out.print(beers + " bottle" + s + " of beer on the wall,");
+                out.println(beers + " bottle" + s + " of beer,");
                 out.print("Go to the store, buy some more,");
                 out.println("99 bottles of beer on the wall.\r\n");
                 return;
-            } else
+            } else {
+                out.print(beers + " bottle" + s + " of beer on the wall,");
+                out.println(beers + " bottle" + s + " of beer,");
                 out.print("Take one down, pass it around,");
+            }
             s = (--beers == 1) ? "" : "s";
             out.println(beers + " bottle" + s + " of beer on the wall.\r\n");
         }
