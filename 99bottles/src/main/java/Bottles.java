a9c11d853ad7c9491f9381b374ca501c95c72fa0
nolanof <francesco.nolano@yoox.com>
2018-03-27 17:10:52 +0200

    Duplicate verse logic to both branches

--- 99bottles/src/main/java/Bottles.java
+++ 99bottles/src/main/java/Bottles.java
@@ -14,15 +14,16 @@ class Bottles {
                 out.println(beers + " bottle" + s + " of beer,");
                 out.print("Go to the store, buy some more,");
                 out.println("99 bottles of beer on the wall.\r\n");
-
             } else {
-                out.print(beers + " bottle" + s + " of beer on the wall,");
-                out.println(beers + " bottle" + s + " of beer,");
-                out.print("Take one down, pass it around,");
                 if (beers - 1 == 1) {
+                    out.print(beers + " bottle" + s + " of beer on the wall,");
+                    out.println(beers + " bottle" + s + " of beer,");
+                    out.print("Take one down, pass it around,");
                     s = "";
-                }
-                else {
+                } else {
+                    out.print(beers + " bottle" + s + " of beer on the wall,");
+                    out.println(beers + " bottle" + s + " of beer,");
+                    out.print("Take one down, pass it around,");
                     s = "s";
                 }
                 out.println(beers - 1 + " bottle" + s + " of beer on the wall.\r\n");
