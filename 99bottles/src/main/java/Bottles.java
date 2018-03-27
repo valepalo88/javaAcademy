9c1682b29ee920590b2247a623a6853939605043
Giulio Caccin <giulio.caccin@ynap.com>
2018-03-27 11:15:49 +0200

    extract first method

--- 99bottles/src/main/java/Bottles.java
+++ 99bottles/src/main/java/Bottles.java
@@ -1,17 +1,23 @@
+import java.io.PrintStream;
+
 class Bottles {
     public static void main(String args[]) {
+        generateOutput(System.out);
+    }
+
+    public static void generateOutput(PrintStream out) {
         String s = "s";
         for (int beers = 99; beers > -1; ) {
-            System.out.print(beers + " bottle" + s + " of beer on the wall,");
-            System.out.println(beers + " bottle" + s + " of beer,");
+            out.print(beers + " bottle" + s + " of beer on the wall,");
+            out.println(beers + " bottle" + s + " of beer,");
             if (beers == 0) {
-                System.out.print("Go to the store, buy some more,");
-                System.out.println("99 bottles of beer on the wall.\r\n");
+                out.print("Go to the store, buy some more,");
+                out.println("99 bottles of beer on the wall.\r\n");
                 return;
             } else
-                System.out.print("Take one down, pass it around,");
+                out.print("Take one down, pass it around,");
             s = (--beers == 1) ? "" : "s";
-            System.out.println(beers + " bottle" + s + " of beer on the wall.\r\n");
+            out.println(beers + " bottle" + s + " of beer on the wall.\r\n");
         }
     }
 }
