9ff2021199ef1d3e0a5cf4680afa782fa85abcf6
Giulio Caccin <giulio.caccin@ynap.com>
2018-03-27 11:07:14 +0200

    add initial example

---
+++ 99bottles/src/main/java/Bottles.java
@@ -0,0 +1,17 @@
+class Bottles {
+    public static void main(String args[]) {
+        String s = "s";
+        for (int beers = 99; beers > -1; ) {
+            System.out.print(beers + " bottle" + s + " of beer on the wall,");
+            System.out.println(beers + " bottle" + s + " of beer,");
+            if (beers == 0) {
+                System.out.print("Go to the store, buy some more,");
+                System.out.println("99 bottles of beer on the wall.\r\n");
+                return;
+            } else
+                System.out.print("Take one down, pass it around,");
+            s = (--beers == 1) ? "" : "s";
+            System.out.println(beers + " bottle" + s + " of beer on the wall.\r\n");
+        }
+    }
+}
