221a2c0ef1bd2d8af16942a0a5dcb6de43dce290
nolanof <francesco.nolano@yoox.com>
2018-03-27 17:12:47 +0200

    Remove useless else.

--- 99bottles/src/main/java/Bottles.java
+++ 99bottles/src/main/java/Bottles.java
@@ -14,18 +14,17 @@ class Bottles {
                 out.println(beers + " bottle" + s + " of beer,");
                 out.print("Go to the store, buy some more,");
                 out.println("99 bottles of beer on the wall.\r\n");
+            } else if (beers == 2) {
+                out.print(beers + " bottle" + s + " of beer on the wall,");
+                out.println(beers + " bottle" + s + " of beer,");
+                out.print("Take one down, pass it around,");
+                s = "";
+                out.println(beers - 1 + " bottle" + s + " of beer on the wall.\r\n");
             } else {
-                if (beers == 2) {
-                    out.print(beers + " bottle" + s + " of beer on the wall,");
-                    out.println(beers + " bottle" + s + " of beer,");
-                    out.print("Take one down, pass it around,");
-                    s = "";
-                } else {
-                    out.print(beers + " bottle" + s + " of beer on the wall,");
-                    out.println(beers + " bottle" + s + " of beer,");
-                    out.print("Take one down, pass it around,");
-                    s = "s";
-                }
+                out.print(beers + " bottle" + s + " of beer on the wall,");
+                out.println(beers + " bottle" + s + " of beer,");
+                out.print("Take one down, pass it around,");
+                s = "s";
                 out.println(beers - 1 + " bottle" + s + " of beer on the wall.\r\n");
             }
         }
