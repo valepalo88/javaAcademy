2cd284b3a61f9c09d897a5e684eba1f16901bacf
nolanof <francesco.nolano@yoox.com>
2018-03-27 18:40:12 +0200

    Extract Verse class

--- 99bottles/src/main/java/Bottles.java
+++ 99bottles/src/main/java/Bottles.java
@@ -1,10 +1,7 @@
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintStream;
 
 class Bottles {
-
-    static final int maxNumber = 99;
 
     public static void main(String args[]) throws FileNotFoundException {
         generateOutput(System.out);
@@ -12,35 +9,9 @@ class Bottles {
 
     public static void generateOutput(PrintStream out) {
 
-        for (int number = maxNumber; number > -1; number--) {
-            out.print(number + getContainer(number) + " of " + getDrink() + " on the wall,");
-            out.println(number + getContainer(number) + " of " + getDrink() + ",");
-            out.print(action(number));
-            out.println(remainingDrinks(number) + getContainer(remainingDrinks(number)) + " of " + getDrink() + " on the wall.\r\n");
+        for (int number = Verse.maxNumber; number > -1; number--) {
+            Verse.getVerse(out, number);
         }
     }
 
-    private static String action(int n) {
-        if (n == 0)
-            return "Go to the store, buy some more,";
-        return "Take one down, pass it around,";
-    }
-
-    private static int remainingDrinks(int n) {
-        if (n == 0) {
-            return maxNumber;
-        }
-        return n - 1;
-    }
-
-    private static String getDrink() {
-        return "beer";
-    }
-
-    private static String getContainer(int n) {
-        if (n == 1)
-            return " bottle";
-
-        return " bottles";
-    }
 }
