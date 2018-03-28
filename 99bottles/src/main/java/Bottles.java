31474694ff9868cbb09d770d2ea366a5e5d1558a
nolanof <francesco.nolano@yoox.com>
2018-03-28 12:52:20 +0200

    Remove unnecessary variable

--- 99bottles/src/main/java/Bottles.java
+++ 99bottles/src/main/java/Bottles.java
@@ -6,7 +6,6 @@ class Bottles {
     }
 
     public static void writeLyrics(PrintStream out) {
-        String s = "s";
         for (int beers = 99; beers > -1; beers--) {
             if (beers == 0) {
                 out.print(beers + " bottles" + " of beer on the wall,");
@@ -18,19 +17,16 @@ class Bottles {
                 out.print(beers + " bottle" + " of beer on the wall,");
                 out.println(beers + " bottle" + " of beer,");
                 out.print("Take one down, pass it around,");
-                s = "";
                 out.println(beers - 1 + " bottles" + " of beer on the wall.\r\n");
             } else if (beers - 1 == 1) {
                 out.print(beers + " bottles" + " of beer on the wall,");
                 out.println(beers + " bottles" + " of beer,");
                 out.print("Take one down, pass it around,");
-                s = "";
                 out.println(beers - 1 + " bottle" + " of beer on the wall.\r\n");
             } else {
                 out.print(beers + " bottles" + " of beer on the wall,");
                 out.println(beers + " bottles" + " of beer,");
                 out.print("Take one down, pass it around,");
-                s = "s";
                 out.println(beers - 1 + " bottles" + " of beer on the wall.\r\n");
             }
         }
