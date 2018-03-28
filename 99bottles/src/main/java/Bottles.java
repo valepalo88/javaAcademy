3c18e84ad531e6bb8169a9716c18bb73c7815e7c
nolanof <francesco.nolano@yoox.com>
2018-03-28 12:33:44 +0200

    Remove braces from else statement

--- 99bottles/src/main/java/Bottles.java
+++ 99bottles/src/main/java/Bottles.java
@@ -14,21 +14,18 @@ class Bottles {
                 out.print("Go to the store, buy some more,");
                 out.println("99 bottles of beer on the wall.\r\n");
                 return;
+            } else if (beers - 1 == 1) {
+                out.print(beers + " bottle" + s + " of beer on the wall,");
+                out.println(beers + " bottle" + s + " of beer,");
+                out.print("Take one down, pass it around,");
+                s = "";
+                out.println(beers - 1 + " bottle" + s + " of beer on the wall.\r\n");
             } else {
-                if (beers - 1 == 1) {
-                    out.print(beers + " bottle" + s + " of beer on the wall,");
-                    out.println(beers + " bottle" + s + " of beer,");
-                    out.print("Take one down, pass it around,");
-                    s = "";
-                    out.println(beers - 1 + " bottle" + s + " of beer on the wall.\r\n");
-                } else {
-                    out.print(beers + " bottle" + s + " of beer on the wall,");
-                    out.println(beers + " bottle" + s + " of beer,");
-                    out.print("Take one down, pass it around,");
-                    s = "s";
-                    out.println(beers - 1 + " bottle" + s + " of beer on the wall.\r\n");
-                }
-
+                out.print(beers + " bottle" + s + " of beer on the wall,");
+                out.println(beers + " bottle" + s + " of beer,");
+                out.print("Take one down, pass it around,");
+                s = "s";
+                out.println(beers - 1 + " bottle" + s + " of beer on the wall.\r\n");
             }
         }
     }
