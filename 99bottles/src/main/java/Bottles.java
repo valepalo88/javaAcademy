0b981672f46cda8b0abebf147d447bb1f0717be5
nolanof <francesco.nolano@yoox.com>
2018-03-28 12:31:46 +0200

    Continue bringing together similar things

--- 99bottles/src/main/java/Bottles.java
+++ 99bottles/src/main/java/Bottles.java
@@ -15,12 +15,20 @@ class Bottles {
                 out.println("99 bottles of beer on the wall.\r\n");
                 return;
             } else {
-                out.print(beers + " bottle" + s + " of beer on the wall,");
-                out.println(beers + " bottle" + s + " of beer,");
-                out.print("Take one down, pass it around,");
-                if (beers - 1 == 1) s = "";
-                else s = "s";
-                out.println(beers - 1 + " bottle" + s + " of beer on the wall.\r\n");
+                if (beers - 1 == 1) {
+                    out.print(beers + " bottle" + s + " of beer on the wall,");
+                    out.println(beers + " bottle" + s + " of beer,");
+                    out.print("Take one down, pass it around,");
+                    s = "";
+                    out.println(beers - 1 + " bottle" + s + " of beer on the wall.\r\n");
+                } else {
+                    out.print(beers + " bottle" + s + " of beer on the wall,");
+                    out.println(beers + " bottle" + s + " of beer,");
+                    out.print("Take one down, pass it around,");
+                    s = "s";
+                    out.println(beers - 1 + " bottle" + s + " of beer on the wall.\r\n");
+                }
+
             }
         }
     }
