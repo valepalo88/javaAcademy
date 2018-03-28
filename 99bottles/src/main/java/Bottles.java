566f78bdc215c3b9002fa55ef06a2dd32bc7c43a
nolanof <francesco.nolano@yoox.com>
2018-03-28 12:53:10 +0200

    Remove unnecessary return

--- 99bottles/src/main/java/Bottles.java
+++ 99bottles/src/main/java/Bottles.java
@@ -12,7 +12,7 @@ class Bottles {
                 out.println(beers + " bottles" + " of beer,");
                 out.print("Go to the store, buy some more,");
                 out.println("99 bottles of beer on the wall.\r\n");
-                return;
+                
             } else if (beers == 1) {
                 out.print(beers + " bottle" + " of beer on the wall,");
                 out.println(beers + " bottle" + " of beer,");
