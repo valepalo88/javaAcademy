69e0dabf89c2e50ee896345375a68a04ef756a9b
nolanof <francesco.nolano@yoox.com>
2018-03-28 14:49:05 +0200

    Remove primitive obsession and feature envy

--- 99bottles/src/main/java/Bottles.java
+++ 99bottles/src/main/java/Bottles.java
@@ -1,3 +1,5 @@
+import domain.Drink;
+
 import java.io.PrintStream;
 
 class Bottles {
@@ -7,28 +9,11 @@ class Bottles {
 
     public static void writeLyrics(PrintStream out) {
         for (int number = 99; number > -1; number--) {
-            out.print(number + container(number) + " of beer on the wall,");
-            out.println(number + container(number) + " of beer,");
-            out.print(action(number));
-            out.println(next(number) + container(next(number)) + " of beer on the wall.\r\n");
+            out.print(number + new Drink(number).container() + " of beer on the wall,");
+            out.println(number + new Drink(number).container() + " of beer,");
+            out.print(new Drink(number).action());
+            out.println(new Drink(number).next() + new Drink(new Drink(number).next()).container() + " of beer on the wall.\r\n");
         }
     }
 
-    private static int next(int beers) {
-        if (beers == 0)
-            return 99;
-        return beers - 1;
-    }
-
-    private static String action(int n) {
-        if (n == 0)
-            return "Go to the store, buy some more,";
-        return "Take one down, pass it around,";
-    }
-
-    private static String container(int n) {
-        if (n == 1)
-            return " bottle";
-        return " bottles";
-    }
 }
