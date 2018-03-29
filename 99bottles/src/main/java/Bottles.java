7243f9da31fa816c8f715b6dce6def74b578a739
valepalo88 <palomba.valeria@yahoo.it>
2018-03-29 14:53:16 +0200

    extract variable out

--- 99bottles/src/main/java/Bottles.java
+++ 99bottles/src/main/java/Bottles.java
@@ -1,3 +1,5 @@
+import java.io.PrintStream;
+
 public class Bottles {
     public static void main(String args[]) {
         writeLyrics();
@@ -6,16 +8,17 @@ public class Bottles {
     public static void writeLyrics() {
         String s = "s";
         for (int beers = 99; beers > -1; beers--) {
-            System.out.print(beers + " bottle" + s + " of beer on the wall,");
-            System.out.println(beers + " bottle" + s + " of beer,");
+            final PrintStream out = System.out;
+            out.print(beers + " bottle" + s + " of beer on the wall,");
+            out.println(beers + " bottle" + s + " of beer,");
             if (beers == 0) {
-                System.out.print("Go to the store, buy some more,");
-                System.out.println("99 bottles of beer on the wall.\r\n");
+                out.print("Go to the store, buy some more,");
+                out.println("99 bottles of beer on the wall.\r\n");
                 return;
             } else
-                System.out.print("Take one down, pass it around,");
+                out.print("Take one down, pass it around,");
             s = (beers - 1 == 1) ? "" : "s";
-            System.out.println(beers - 1 + " bottle" + s + " of beer on the wall.\r\n");
+            out.println(beers - 1 + " bottle" + s + " of beer on the wall.\r\n");
         }
     }
 }
