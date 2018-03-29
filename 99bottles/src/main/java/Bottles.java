859888729bf2c9b81fe4cc368864954b94f8214d
valepalo88 <palomba.valeria@yahoo.it>
2018-03-29 16:19:39 +0200

    Simplify condition

--- 99bottles/src/main/java/Bottles.java
+++ 99bottles/src/main/java/Bottles.java
@@ -7,30 +7,27 @@ public class Bottles {
 
     public static void writeLyrics(PrintStream out) {
         String s = "s";
-        for (int beers = 99; beers > -1; beers--) {
+        for (int beers = 99; beers > -1; beers--)
             if (beers == 0) {
                 out.print(beers + " bottle" + s + " of beer on the wall,");
                 out.println(beers + " bottle" + s + " of beer,");
                 out.print("Go to the store, buy some more,");
                 out.println("99 bottles of beer on the wall.\r\n");
                 return;
-            } else
-            {
-                if (beers - 1 == 1)
-                {
+            } else {
+                if (beers == 2) {
                     out.print(beers + " bottle" + s + " of beer on the wall,");
                     out.println(beers + " bottle" + s + " of beer,");
                     out.print("Take one down, pass it around,");
                     s = "";
-                }
-                else {
+                    out.println(beers - 1 + " bottle" + s + " of beer on the wall.\r\n");
+                } else {
                     out.print(beers + " bottle" + s + " of beer on the wall,");
                     out.println(beers + " bottle" + s + " of beer,");
                     out.print("Take one down, pass it around,");
                     s = "s";
+                    out.println(beers - 1 + " bottle" + s + " of beer on the wall.\r\n");
                 }
-                out.println(beers - 1 + " bottle" + s + " of beer on the wall.\r\n");
             }
-        }
     }
 }
