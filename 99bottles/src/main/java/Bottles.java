8fb68c00f0b363bf1a6339d348745e555734ee96
valepalo88 <palomba.valeria@yahoo.it>
2018-03-29 17:01:23 +0200

    remove duplication

--- 99bottles/src/main/java/Bottles.java
+++ 99bottles/src/main/java/Bottles.java
@@ -8,31 +8,20 @@ public class Bottles {
     public static void writeLyrics(PrintStream out) {
         for (int beers = 99; beers > -1; beers--)
             if (beers == 0) {
-                out.print(beers + " bottles"  + " of beer on the wall,");
-                out.println(beers + " bottles"  + " of beer,");
+                out.print(beers + " bottles" + " of beer on the wall,");
+                out.println(beers + " bottles" + " of beer,");
                 out.print("Go to the store, buy some more,");
                 out.println("99 bottles of beer on the wall.\r\n");
-            }else if(beers==1){
-                out.print(beers + " bottle"  + " of beer on the wall,");
-                out.println(beers + " bottle"  + " of beer,");
-                out.print("Take one down, pass it around,");
-                out.println(beers - 1 + " bottles"  + " of beer on the wall.\r\n");
-            }
-            else if (beers == 2) {
-                out.print(beers + " bottles"  + " of beer on the wall,");
-                out.println(beers + " bottles"  + " of beer,");
-                out.print("Take one down, pass it around,");
-                out.println(beers - 1 + getContainer(beers - 1)  + " of beer on the wall.\r\n");
             } else {
-                out.print(beers + " bottles"  + " of beer on the wall,");
-                out.println(beers + " bottles"  + " of beer,");
+                out.print(beers + getContainer(beers) + " of beer on the wall,");
+                out.println(beers + getContainer(beers) + " of beer,");
                 out.print("Take one down, pass it around,");
                 out.println(beers - 1 + getContainer(beers - 1) + " of beer on the wall.\r\n");
             }
     }
 
     public static String getContainer(int beers) {
-        if (beers==1)
+        if (beers == 1)
             return " bottle";
         return " bottles";
     }
