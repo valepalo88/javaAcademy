ea84adfb0e63534f393051df07858736a5f6257a
valepalo88 <palomba.valeria@yahoo.it>
2018-03-29 16:26:40 +0200

    remove return statement

--- 99bottles/src/main/java/Bottles.java
+++ 99bottles/src/main/java/Bottles.java
@@ -13,7 +13,6 @@ public class Bottles {
                 out.println(beers + " bottle" + s + " of beer,");
                 out.print("Go to the store, buy some more,");
                 out.println("99 bottles of beer on the wall.\r\n");
-                return;
             } else {
                 if (beers == 2) {
                     out.print(beers + " bottle" + s + " of beer on the wall,");
