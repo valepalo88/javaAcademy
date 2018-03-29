9b82bbc8d42a787974acaa6d035ce7a13806a4b0
valepalo88 <palomba.valeria@yahoo.it>
2018-03-29 17:17:48 +0200

    extract method getAction

--- 99bottles/src/main/java/Bottles.java
+++ 99bottles/src/main/java/Bottles.java
@@ -8,16 +8,22 @@ public class Bottles {
     public static void writeLyrics(PrintStream out) {
         for (int beers = 99; beers > -1; beers--)
             if (beers == 0) {
-                out.print(beers + " bottles" + " of beer on the wall,");
-                out.println(beers + " bottles" + " of beer,");
-                out.print("Go to the store, buy some more,");
+                out.print(beers + getContainer(beers) + " of beer on the wall,");
+                out.println(beers + getContainer(beers) + " of beer,");
+                out.print(getAction(beers));
                 out.println("99 bottles of beer on the wall.\r\n");
             } else {
                 out.print(beers + getContainer(beers) + " of beer on the wall,");
                 out.println(beers + getContainer(beers) + " of beer,");
-                out.print("Take one down, pass it around,");
+                out.print(getAction(beers));
                 out.println(beers - 1 + getContainer(beers - 1) + " of beer on the wall.\r\n");
             }
+    }
+
+    public static String getAction(int beers) {
+        if (beers == 0)
+            return "Go to the store, buy some more,";
+        return "Take one down, pass it around,";
     }
 
     public static String getContainer(int beers) {
