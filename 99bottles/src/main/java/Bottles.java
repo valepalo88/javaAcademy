c573c0f6b40b5fc5eaf6ce4a9d6a6cadd2733c3d
valepalo88 <palomba.valeria@yahoo.it>
2018-03-29 16:16:11 +0200

    Move common lines inside second condition

--- 99bottles/src/main/java/Bottles.java
+++ 99bottles/src/main/java/Bottles.java
@@ -16,12 +16,19 @@ public class Bottles {
                 return;
             } else
             {
-                out.print(beers + " bottle" + s + " of beer on the wall,");
-                out.println(beers + " bottle" + s + " of beer,");
-                out.print("Take one down, pass it around,");
                 if (beers - 1 == 1)
+                {
+                    out.print(beers + " bottle" + s + " of beer on the wall,");
+                    out.println(beers + " bottle" + s + " of beer,");
+                    out.print("Take one down, pass it around,");
                     s = "";
-                else s = "s";
+                }
+                else {
+                    out.print(beers + " bottle" + s + " of beer on the wall,");
+                    out.println(beers + " bottle" + s + " of beer,");
+                    out.print("Take one down, pass it around,");
+                    s = "s";
+                }
                 out.println(beers - 1 + " bottle" + s + " of beer on the wall.\r\n");
             }
         }
