adcf06142e271188199e54973dec72b68b6b0259
valepalo88 <palomba.valeria@yahoo.it>
2018-03-29 16:05:12 +0200

    Move common lines inside condition

--- 99bottles/src/main/java/Bottles.java
+++ 99bottles/src/main/java/Bottles.java
@@ -8,14 +8,18 @@ public class Bottles {
     public static void writeLyrics(PrintStream out) {
         String s = "s";
         for (int beers = 99; beers > -1; beers--) {
-            out.print(beers + " bottle" + s + " of beer on the wall,");
-            out.println(beers + " bottle" + s + " of beer,");
             if (beers == 0) {
+                out.print(beers + " bottle" + s + " of beer on the wall,");
+                out.println(beers + " bottle" + s + " of beer,");
                 out.print("Go to the store, buy some more,");
                 out.println("99 bottles of beer on the wall.\r\n");
                 return;
             } else
+            {
+                out.print(beers + " bottle" + s + " of beer on the wall,");
+                out.println(beers + " bottle" + s + " of beer,");
                 out.print("Take one down, pass it around,");
+            }
             s = (beers - 1 == 1) ? "" : "s";
             out.println(beers - 1 + " bottle" + s + " of beer on the wall.\r\n");
         }
